be1f340d266ca0fbbe91907ad0d8219216acf97c
Michael Kamensky <mkamensk@ucsc.edu>
2023-04-17 11:53:55 -0700

    checkpoint for the graph ADT

--- pa2/Graph.c
+++ pa2/Graph.c
@@ -12,31 +12,95 @@
 
 // structs --------------------------------------------------------------------
 
-// private Graph type
-typedef struct GraphObj* Graph;
-
 // private GraphObj type
 typedef struct GraphObj{
-   ListElement data;
-   Graph next;
-   Graph previous;
+   // int for the most recently used source
+   int source;
+   // number of vertecies
+   int vertecies;
+   // number of edges
+   int edges;
+   // neighbors list
+   List *neighbors;
+   // the array for the color
+   int *color; 
+   // the array for parents
+   int *parents;
+   // the array for distance
+   int *distances;
 } GraphObj;
-
-// private ListObj type
-typedef struct ListObj{
-   Graph front;
-   Graph back;
-   int index;
-   Graph cursor;
-   int length;
-} ListObj;
 
 
 
 // Constructors-Destructors ---------------------------------------------------
 
+// creates a new Graph 
+Graph newGraph(int n) {
+   Graph G = malloc(sizeof(GraphObj));
+   G->vertecies = n;
+   G->edges = 0;
+   G->source = NIL;
+
+   G->neighbors = calloc(n+1, sizeof(List));
+   G->color = calloc(n+1, sizeof(int));
+   G->parents = calloc(n+1, sizeof(int));
+   G->distances = calloc(n+1, sizeof(int));
+
+}
+
 
 // Access functions -----------------------------------------------------------
+
+// returns the number of verticies 
+int getOrder(Graph G) {
+   if (G != NULL) {
+      return G->vertecies;
+   } 
+}
+
+// retruns the number of edges
+int getSize(Graph G) {
+   if (G != NULL) {
+      return G->edges;
+   }
+}
+
+// returns the source vertex
+int getSource(Graph G) {
+   if (G != NULL) {
+      return G->source;
+   }
+}
+
+// return the parents of the vertex number u
+int getParent(Graph G, int u) {
+   if (G != NULL && u > NIL) {
+      return G->parents[u];
+   }
+}
+
+// returns the dist from the source node to the node u
+int getDist(Graph G, int u) {
+   if (G != NULL && u > NIL) {
+      return G->distances[u];
+   }
+}
+
+// this func returns the path 
+void getPath(List L, Graph G, int u) {
+   // logic from print path should be the same but we simply write to the list
+   if (G->source == u) {
+        append(L, G->source);
+   } 
+   else if (G->parents[u] == NIL) {
+      printf("%d is not reachable from %d")
+   }
+   else {
+      PrintPath(G, G->source, G->parents[u]);
+      append(L, u);
+   }
+   
+}
 
 
 // Manipulation procedures ----------------------------------------------------
--- pa2/Graph.h
+++ pa2/Graph.h
@@ -2,9 +2,10 @@
 // Graph.h
 // Header file for Graph ADT
 //-----------------------------------------------------------------------------
-#ifndef Graph_H_INCLUDE_
-#define Graph_H_INCLUDE_
-#include<stdbool.h>
+
+#define INF -1  //defines INF for the FBS
+#define NIL 0  //defines NIL for the FBS
+
 
 // Exported types -------------------------------------------------------------
 typedef struct GraphObj* Graph;
@@ -12,6 +13,7 @@ typedef struct GraphObj* Graph;
 /*** Constructors-Destructors ***/
 Graph newGraph(int n);
 void freeGraph(Graph* pG);
+
 /*** Access functions ***/
 int getOrder(Graph G);
 int getSize(Graph G);
@@ -19,12 +21,12 @@ int getSource(Graph G);
 int getParent(Graph G, int u);
 int getDist(Graph G, int u);
 void getPath(List L, Graph G, int u);
+
 /*** Manipulation procedures ***/
 void makeNull(Graph G);
 void addEdge(Graph G, int u, int v);
 void addArc(Graph G, int u, int v);
 void BFS(Graph G, int s);
+
 /*** Other operations ***/
 void printGraph(FILE* out, Graph G);
-
-#endif
