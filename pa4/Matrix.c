9403ae722164e3def22c13c231a72a66986f2422
Michael Kamensky <mkamensk@ucsc.edu>
2023-05-02 21:15:57 -0700

    removed the rounding function

--- pa4/Matrix.c
+++ pa4/Matrix.c
@@ -396,10 +396,12 @@ Matrix product(Matrix A, Matrix B) {
     return AB; 
 }
 
+#if 0
 static double round_one_decimal(double val) {
     double rounded_down = floor(val * 100) / 100;
     return rounded_down;
 }
+#endif
 
 // printMatrix()
 // Prints a string representation of Matrix M to filestream out. Zero rows
