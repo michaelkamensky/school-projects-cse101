18b050c67d33cfcf2c59c0f20f1bfface2fd40f9
Michael Kamensky <mkamensk@ucsc.edu>
2023-06-03 21:51:13 -0700

    working version of wordfrequency
    need to run tests and debug it

--- pa8/WordFrequency.cpp
+++ pa8/WordFrequency.cpp
@@ -1,69 +1,91 @@
-/********************************************************************************* 
-* Michael V Kamensky
-* 2023 Winter CSE101 PA#7
-* Order.cpp 
-* A main class that uses the dictionary ADT to construct and dictionary and return an inorder and preorder string representation
-*********************************************************************************/ 
 #include<iostream>
+#include<fstream>
 #include<string>
-#include<stdexcept>
-#include <fstream>
-#include"Dictionary.h"
+#include<cstring>
 
-#define DEBUG 0
+#include"Dictionary.h"
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
-    if (argc < 3) {
-        cout << "Usage: ./program input_file output_file" << endl;
-        return 1;
-    }
-   
-    const string inputFilename = argv[1];
-    const string outputFilename = argv[2];
-   
-    ifstream inputFile(inputFilename);
-    ofstream outputFile(outputFilename);
-   
-    if (!inputFile.is_open()) {
-        cout << "Failed to open the input file." << endl;
-        return 1;
-    }
-   
-    if (!outputFile.is_open()) {
-        cout << "Failed to open the output file." << endl;
-        return 1;
-    }
+#define MAX_LEN 300
 
-    // creating dictionary
-    Dictionary ret = Dictionary();
-   
-    // reading the file and then adding it to the dictionary 
-    std::string line;
-    int count = 1;
-    while (std::getline(inputFile, line)) {
-        //std::cout << line << std::endl;
-        ret.setValue(line, count);
-        count += 1;
-    }
+int main(int argc, char * argv[]){
 
-    // now printing in order representation
-    outputFile << ret << endl;
+   int line_count;
+   size_t begin, end, len;
+   ifstream in;
+   ofstream out;
+   string line;
+   string tokenBuffer;
+   string token;
+   // string delim = " "; 
+   string delim = " \t\\\"\',<.>/?;:[{]}|`~!@#$^&*()-_=+0123456789";
 
-    // now printing out pre order representation
-    outputFile << ret.pre_string() << endl;
+   // check command line for correct number of arguments
+   if( argc != 3 ){
+      cerr << "Usage: " << argv[0] << " <input file> <output file>" << endl;
+      return(EXIT_FAILURE);
+   }
 
-#if DEBUG
-    cout <<  "str1 " << str1 << endl;
-    cout <<  "str2 " << str2 << endl;
+   // open files for reading and writing 
+   in.open(argv[1]);
+   if( !in.is_open() ){
+      cerr << "Unable to open file " << argv[1] << " for reading" << endl;
+      return(EXIT_FAILURE);
+   }
+
+   out.open(argv[2]);
+   if( !out.is_open() ){
+      cerr << "Unable to open file " << argv[2] << " for writing" << endl;
+      return(EXIT_FAILURE);
+   }
+
+   // read each line of input file, then count and print tokens 
+   line_count = 0;
+   Dictionary dict;
+   while( getline(in, line) )  {
+      line_count++;
+      len = line.length();
+      
+      // get first token
+      begin = min(line.find_first_not_of(delim, 0), len);
+      end   = min(line.find_first_of(delim, begin), len);
+      token = line.substr(begin, end-begin);
+      
+      while( token!="" ){  // we have a token
+        
+        // making token lower case
+        for (char& c : token) {
+            c = std::tolower(c);
+        }
+        // logic to add to the dictionary
+        if (dict.contains(token)) {
+            int num = dict.getValue(token);
+            num += 1;
+            dict.setValue(token, num);
+        } else {
+            dict.setValue(token, 1);
+        }
+
+         // get next token
+         begin = min(line.find_first_not_of(delim, end+1), len);
+         end   = min(line.find_first_of(delim, begin), len);
+         token = line.substr(begin, end-begin);
+      }
+#if 0
+      // print tokens in this line
+      out << "line " << line_count << " contains " << token_count;
+      out << " token" << (token_count==1?"":"s") << endl;
+      out << tokenBuffer << endl;
 #endif
+   }
 
+   // logic to print out the dictionary
+   cout << dict << endl;
 
-   
-    // closing the files
-    inputFile.close();
-    outputFile.close();
+   // close files 
+   in.close();
+   out.close();
 
-    return 0;
+   return(EXIT_SUCCESS);
 }
